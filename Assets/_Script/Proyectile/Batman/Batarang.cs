bb6c822905296c8c95ffe7982bd015ab7616ad74
Germanchrystan <germanchrystan@gmail.com>
2022-12-27 01:34:03 -0300

    Batarang spawn

--- Assets/_Script/Proyectile/Batman/Batarang.cs
+++ Assets/_Script/Proyectile/Batman/Batarang.cs
@@ -4,17 +4,54 @@ using UnityEngine;
 
 public class Batarang : MonoBehaviour
 {
+    private Rigidbody2D rigidbody;
+    public float speed = 100f;
     public Vector2 direction;
+    
+    private float _startingTime;
+    public float livingTime = 1f;
+
+    public bool returning = false;
     public Transform returnPoint;
-    // Start is called before the first frame update
+ 
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+    
     void Start()
     {
-        
+        _startingTime = Time.time;
+        Invoke("Return", livingTime);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate() 
     {
-        
+        if (!returning)
+        {
+            Vector2 movement = direction.normalized * speed;
+            rigidbody.velocity = movement;
+        }
+        else 
+        {
+            direction = new Vector2(returnPoint.position.x - transform.position.x, returnPoint.position.y - transform.position.y);
+            Vector2 movement = direction.normalized * speed;
+            rigidbody.velocity = movement;
+        }
+
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(returning)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    private void Return()
+    {
+        returning = true;
+    }
+    
 }
