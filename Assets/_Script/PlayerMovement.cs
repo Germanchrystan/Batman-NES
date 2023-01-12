f96be92a5fbe1fc3b4738f35a04db7dde6640bc4
Germanchrystan <germanchrystan@gmail.com>
2023-01-12 20:41:27 -0300

    added a little bit more release

--- Assets/_Script/PlayerMovement.cs
+++ Assets/_Script/PlayerMovement.cs
@@ -262,7 +262,7 @@ public class PlayerMovement:MonoBehaviour
 		{
             rg.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
         } 
-		else if (rg.velocity.y > 0 && !Input.GetButton("Jump")) 
+		else if ((rg.velocity.y > 0 && !Input.GetButton("Jump")) || (isWallJumping && !Input.GetButton("Jump"))) 
 		{
             rg.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
         }
